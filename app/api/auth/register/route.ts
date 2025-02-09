8a7623dce53d4eea5fefaca90b6c389e323b88f2
abolfazl-khalaj <abolfazlkhalaj.dev@gmail.com>
2025-02-09 20:03:43 +0330

    feat: valid data user status register

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,12 +1,28 @@
 
 import connectedDB from "@/configs/database/db";
+import Joi from "joi";
 import { NextRequest, NextResponse } from "next/server";
 
-export default await function POST(req:NextRequest):NextResponse{
+export default async function POST(req:NextRequest):Promise<NextResponse>{
 
     try{
-        connectedDB()
+        await connectedDB()
 
+        const body = await req.json()
+
+        const schema = Joi.object({
+            name: Joi.string().trim().required(),
+            phone: Joi.string()
+            .pattern(/^(09)(1[0-9]|2[0-2]|3[0-9]|9[0-9])-?[0-9]{3}-?[0-9]{4}$/).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().min(8).required(),
+          });
+
+        const {error} = schema.validate(body)
+
+        if(error){
+            return NextResponse.json({messageError : error.details[0].message},{status:402})
+        }
 
 
         return NextResponse.json({message: 'successfully ..'})
